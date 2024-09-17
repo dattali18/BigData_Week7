8a451d41e9b9076d002200abb70f8cdd50318b9a
Daniel Attali <danielattali16@gmail.com>
2024-09-17 18:55:22 +0300

    finished second part

--- scripts/queries.R
+++ scripts/queries.R
@@ -6,9 +6,69 @@ con <- dbConnect(RSQLite::SQLite(), "BX_db.sqlite")
 
 
 # Check the number of records in each table
-print(dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Users'"))
-print(dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Books'"))
-print(dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Book-Ratings'"))
+
+users.length <- dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Users'")
+books.length <- dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Books'")
+ratings.length <- dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Book-Ratings'")
+
+# a. Number of users
+print(paste("number of users: ", users.length))
+# b. Number of books
+print(paste("number of books: ", books.length))
+# c. Number of ratings
+print(paste("number of ratings: ", ratings.length))
+
+# d. Histogram of user-ratings (how many users have rated N times?)
+dbExecute(con, "DROP VIEW IF EXISTS userHistogram;")
+dbExecute(con, "CREATE VIEW userHistogram AS
+SELECT COUNT(*) AS num_ratings
+FROM 'BX-Users'
+NATURAL JOIN 'BX-Book-Ratings'
+GROUP BY 'User-ID'
+ORDER BY COUNT(*);")
+
+histogram.user.ratings <- dbGetQuery(con, "SELECT num_ratings, COUNT(*) AS bin_size FROM userHistogram GROUP BY num_ratings ORDER BY num_ratings;")
+
+dbExecute(con, "DROP VIEW userHistogram;")
+
+print("Histogram of user-ratings:")
+print(histogram.user.ratings)
+
+# e. Histogram of book-ratings (how many books have been rated N times?)
+
+dbExecute(con, "DROP VIEW IF EXISTS booksHistogram;")
+
+dbExecute(con, "CREATE VIEW booksHistogram AS
+SELECT ISBN, COUNT(*) AS num_ratings
+FROM 'BX-Books'
+NATURAL JOIN 'BX-Book-Ratings'
+GROUP BY ISBN
+ORDER BY COUNT(*);")
+
+histogram.books.ratings <- dbGetQuery(con, "SELECT num_ratings, COUNT(*) AS bin_size FROM booksHistogram GROUP BY num_ratings ORDER BY num_ratings;")
+
+dbExecute(con, "DROP VIEW booksHistogram;")
+
+print("Histogram of book-ratings:")
+print(histogram.books.ratings)
+
+
+# f. Top-10 rated books?
+top.books <- dbGetQuery(con, "SELECT 'Book-Title', COUNT('Book-Rating') AS rating_count
+FROM 'BX-Books'
+NATURAL JOIN 'BX-Book-Ratings'
+GROUP BY 'ISBN'
+ORDER BY rating_count DESC
+LIMIT 10;")
+
+print("Top-10 rated books:")
+print(top.books)
+
+# g. Top-10 rated users?
+top.users <- dbGetQuery(con, "SELECT 'User-ID', COUNT(*) AS num_ratings FROM 'BX-Users' NATURAL JOIN 'BX-Book-Ratings' GROUP BY 'User-ID' ORDER BY num_ratings DESC LIMIT 10;")
+
+print("Top-10 rated users:")
+print(top.users)
 
 # Disconnect when done
 dbDisconnect(con)
--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,34 +1,39 @@
+.headers on
+.mode csv
+
 -- a. How many users?
-SELECT COUNT(*) FROM BX_Users;
+SELECT COUNT(*) FROM "BX-Users";
 
 -- b. How many books?
-SELECT COUNT(*) FROM BX_Books;
+SELECT COUNT(*) FROM "BX-Books";
 
 -- c. How many ratings?
-SELECT COUNT(*) FROM BX_Book_Ratings;
+SELECT COUNT(*) FROM "BX-Book-Ratings";
 
 -- d. Histogram of user-ratings (how many users have rated N times?)
 -- delete the view if exists so there is no error
-DROP VIEW IF EXISTS temp;
+DROP VIEW IF EXISTS usersHistogram;
 
-CREATE VIEW temp AS 
+CREATE VIEW usersHistogram AS 
 SELECT COUNT(*) AS num_ratings
-FROM BX_Users 
-NATURAL JOIN BX_Book_Ratings
+FROM "BX-Users" 
+NATURAL JOIN "BX-Book-Ratings"
 GROUP BY "User-ID"
 ORDER BY COUNT(*);
 
 SELECT num_ratings, COUNT(*) AS bin_size
-FROM temp
+FROM usersHistogram
 GROUP BY num_ratings
 ORDER BY num_ratings;
+
+DROP VIEW usersHistogram;
 
 -- e. Histogram of book-ratings (how many books have been rated N times?)
 DROP VIEW IF EXISTS booksHistogram;
 
 CREATE VIEW booksHistogram AS 
 SELECT COUNT(*) AS num_ratings
-FROM BX_Book_Ratings
+FROM "BX-Book-Ratings"
 GROUP BY "ISBN"
 ORDER BY COUNT(*);
 
@@ -37,17 +42,19 @@ FROM booksHistogram
 GROUP BY num_ratings
 ORDER BY num_ratings;
 
+DROP VIEW booksHistogram;
+
 -- f. Top-10 rated books?
 SELECT "Book-Title", COUNT("Book-Rating") AS rating_count
-FROM BX_Books 
-NATURAL JOIN BX_Book_Ratings
+FROM "BX-Books" 
+NATURAL JOIN "BX-Book-Ratings"
 GROUP BY "ISBN"
 ORDER BY rating_count DESC
 LIMIT 10;
 
 -- g. Top-10 active users?
 SELECT "User-ID", COUNT("User-ID") AS N
-FROM BX_Book_Ratings
+FROM "BX-Book-Ratings"
 GROUP BY "User-ID"
 ORDER BY N DESC
 LIMIT 10;
