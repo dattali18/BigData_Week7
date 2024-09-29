729958c8c0805cec843be715b2a8086ae6f24b27
Daniel Attali <danielattali16@gmail.com>
2024-09-29 17:25:14 +0300

    remade the pdf

--- scripts/queries.R
+++ scripts/queries.R
@@ -1,74 +1,58 @@
-library(DBI)
+# ******************************** 1. Load the Data into R  ********************************
+
+
+# Load necessary libraries
 library(RSQLite)
+library(sqldf)
+library(recommenderlab)
+library(ggplot2)
 
-# Connect to SQLite database
-con <- dbConnect(RSQLite::SQLite(), "BX_db.sqlite")
+# Connect to the SQLite database
+con <- dbConnect(SQLite(), dbname = "BX_db.sqlite")
 
+# Load tables into R
+users <- dbGetQuery(con, "SELECT * FROM 'BX-Users'")
+books <- dbGetQuery(con, "SELECT * FROM 'BX-Books'")
+ratings <- dbGetQuery(con, "SELECT * FROM 'BX-Book-Ratings'")
 
-# Check the number of records in each table
+# Number of users, books, and ratings
+print(cat("Number of users:", nrow(users)))
+print(cat("Number of books:", nrow(books)))
+print(cat("Number of ratings:", nrow(ratings)))
 
-users.length <- dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Users'")
-books.length <- dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Books'")
-ratings.length <- dbGetQuery(con, "SELECT COUNT(*) FROM 'BX-Book-Ratings'")
+# disconnect from the database
 
-# a. Number of users
-print(paste("number of users: ", users.length))
-# b. Number of books
-print(paste("number of books: ", books.length))
-# c. Number of ratings
-print(paste("number of ratings: ", ratings.length))
-
-# d. Histogram of user-ratings (how many users have rated N times?)
-dbExecute(con, "DROP VIEW IF EXISTS userHistogram;")
-dbExecute(con, "CREATE VIEW userHistogram AS
-SELECT COUNT(*) AS num_ratings
-FROM 'BX-Users'
-NATURAL JOIN 'BX-Book-Ratings'
-GROUP BY 'User-ID'
-ORDER BY COUNT(*);")
-
-histogram.user.ratings <- dbGetQuery(con, "SELECT num_ratings, COUNT(*) AS bin_size FROM userHistogram GROUP BY num_ratings ORDER BY num_ratings;")
-
-dbExecute(con, "DROP VIEW userHistogram;")
-
-print("Histogram of user-ratings:")
-print(histogram.user.ratings)
-
-# e. Histogram of book-ratings (how many books have been rated N times?)
-
-dbExecute(con, "DROP VIEW IF EXISTS booksHistogram;")
-
-dbExecute(con, "CREATE VIEW booksHistogram AS
-SELECT ISBN, COUNT(*) AS num_ratings
-FROM 'BX-Books'
-NATURAL JOIN 'BX-Book-Ratings'
-GROUP BY ISBN
-ORDER BY COUNT(*);")
-
-histogram.books.ratings <- dbGetQuery(con, "SELECT num_ratings, COUNT(*) AS bin_size FROM booksHistogram GROUP BY num_ratings ORDER BY num_ratings;")
-
-dbExecute(con, "DROP VIEW booksHistogram;")
-
-print("Histogram of book-ratings:")
-print(histogram.books.ratings)
-
-
-# f. Top-10 rated books?
-top.books <- dbGetQuery(con, "SELECT 'Book-Title', COUNT('Book-Rating') AS rating_count
-FROM 'BX-Books'
-NATURAL JOIN 'BX-Book-Ratings'
-GROUP BY 'ISBN'
-ORDER BY rating_count DESC
-LIMIT 10;")
-
-print("Top-10 rated books:")
-print(top.books)
-
-# g. Top-10 rated users?
-top.users <- dbGetQuery(con, "SELECT 'User-ID', COUNT(*) AS num_ratings FROM 'BX-Users' NATURAL JOIN 'BX-Book-Ratings' GROUP BY 'User-ID' ORDER BY num_ratings DESC LIMIT 10;")
-
-print("Top-10 rated users:")
-print(top.users)
-
-# Disconnect when done
 dbDisconnect(con)
+
+# ***************************** 2. Data Exploration and Preprocessing  **************************
+
+
+# Filter ratings to only include valid entries
+ratings <- sqldf("SELECT * FROM ratings WHERE \"Book-Rating\" > 0 AND \"Book-Rating\" <= 10")
+
+# Filter users and books with at least 30 ratings
+books_with_ratings <- sqldf("SELECT ISBN FROM ratings GROUP BY ISBN HAVING COUNT(*) > 30")
+users_with_ratings <- sqldf("SELECT \"User-ID\" FROM ratings GROUP BY \"User-ID\" HAVING COUNT(*) > 30 AND COUNT(*) < 300")
+
+
+# Keep only these ratings
+ratings <- sqldf("SELECT * FROM ratings WHERE \"User-ID\" IN users_with_ratings AND ISBN IN books_with_ratings")
+
+
+# ******************************** 3. Statistics and Histograms  *******************************
+
+pdf("outputs/ratings_histograms.pdf")
+
+# Histogram of ratings by user
+user_ratings_hist <- sqldf("SELECT COUNT(\"User-ID\") AS ratings_per_user FROM ratings GROUP BY \"User-ID\"")
+ggplot(user_ratings_hist, aes(x = ratings_per_user)) +
+    geom_histogram(binwidth = 5, fill = "blue") +
+    ggtitle("Histogram of Ratings by Users")
+
+# Histogram of ratings by book
+book_ratings_hist <- sqldf("SELECT COUNT(ISBN) AS ratings_per_book FROM ratings GROUP BY ISBN")
+ggplot(book_ratings_hist, aes(x = ratings_per_book)) +
+    geom_histogram(binwidth = 5, fill = "green") +
+    ggtitle("Histogram of Ratings by Books")
+
+dev.off()
